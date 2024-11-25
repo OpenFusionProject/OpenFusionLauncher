62211744cccf853cb2015feb72699f6b46beddf3
Gent Semaj <me@ycc.dev>
2024-11-24 22:26:53 -0800

    Skeleton for account registration

--- app/LoginModal.tsx
+++ app/LoginModal.tsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from "react";
 
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 import Button from "./Button";
 
 import { ServerEntry } from "./types";
+
+const TAB_LOGIN = "login";
+const TAB_REGISTER = "register";
 
 export default function LoginModal({
   server,
@@ -21,32 +26,92 @@ export default function LoginModal({
 
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
 
-  useEffect(() => {
+  const [tab, setTab] = useState<string>(TAB_LOGIN);
+
+  const validateLogin = () => {
+    return username.length > 0 && password.length > 0;
+  }
+
+  const clear = () => {
     setUsername("");
     setPassword("");
+    setConfirmPassword("");
+    setEmail("");
+  };
+
+  useEffect(() => {
+    clear();
   }, [server]);
 
   return (
     <Modal show={show} onHide={() => setShow(false)} centered={true}>
       <Modal.Header>
-        <Modal.Title>Log In</Modal.Title>
+        <Modal.Title>{server?.description}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <br />
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <Tabs
+            activeKey={tab}
+            onSelect={(k) => setTab(k ?? TAB_LOGIN)}
+            className="mb-3"
+            fill
+          >
+            <Tab eventKey={TAB_LOGIN} title="Log In">
+              <Form.Group className="mb-3" controlId="username">
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+            </Tab>
+            <Tab eventKey={TAB_REGISTER} title="Register">
+              <Form.Group className="mb-3" controlId="username">
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="confirmPassword">
+                <Form.Control
+                  type="confirmPassword"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Control
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+            </Tab>
+          </Tabs>
         </Form>
       </Modal.Body>
       <Modal.Footer>
@@ -59,11 +124,13 @@ export default function LoginModal({
         <Button
           onClick={() => {
             setShow(false);
-            onSubmit(server!.uuid!, username, password);
+            if (tab === TAB_LOGIN) {
+              onSubmit(server!.uuid!, username, password);
+            } // TODO: Register
           }}
           variant="success"
-          text="Log In"
-          enabled={username.length > 0 && password.length > 0}
+          text={tab === TAB_LOGIN ? "Log In" : "Register"}
+          enabled={tab === TAB_LOGIN ? validateLogin() : false}
         />
       </Modal.Footer>
     </Modal>
