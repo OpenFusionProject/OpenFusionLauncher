8db030db51d90c388482a64d3e97f27cb0c0fa2d
Gent Semaj <me@ycc.dev>
2024-11-25 13:02:37 -0800

    Tooltips for username + password requirements

--- app/LoginModal.tsx
+++ app/LoginModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
@@ -8,9 +8,17 @@ import Tab from "react-bootstrap/Tab";
 import Button from "./Button";
 
 import { ServerEntry } from "./types";
+import { Overlay, Tooltip } from "react-bootstrap";
 
 const TAB_LOGIN = "login";
 const TAB_REGISTER = "register";
+
+const CONTROL_ID_USERNAME = "username";
+const CONTROL_ID_PASSWORD = "password";
+const CONTROL_ID_NEW_USERNAME = "newUsername";
+const CONTROL_ID_NEW_PASSWORD = "newPassword";
+const CONTROL_ID_CONFIRM_PASSWORD = "confirmPassword";
+const CONTROL_ID_EMAIL = "email";
 
 const validateUsername = (username: string) => {
   // From OpenFusion:
@@ -33,6 +41,18 @@ const validateEmail = (email: string) => {
   return regex.test(email);
 }
 
+function RequirementsTooltip ({ focusedControlId, controlId, children }: { focusedControlId: string | null, controlId: string, children: any }) {
+  const target = document.getElementById(controlId);
+  const show = !!(focusedControlId && focusedControlId === controlId);
+  return (
+    <Overlay target={target} show={show} placement="right">
+      <Tooltip id={controlId + "Tooltip"}>
+        {children}
+      </Tooltip>
+    </Overlay>
+  );
+}
+
 export default function LoginModal({
   server,
   show,
@@ -49,6 +69,9 @@ export default function LoginModal({
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+
+  // I could not get this to work any other way; improvements welcome
+  const [activeControl, setActiveControl] = useState<string | null>(null);
 
   const [tab, setTab] = useState<string>(TAB_LOGIN);
 
@@ -90,60 +113,77 @@ export default function LoginModal({
             fill
           >
             <Tab eventKey={TAB_LOGIN} title="Log In">
-              <Form.Group className="mb-3" controlId="username">
+              <Form.Group className="mb-3" controlId={CONTROL_ID_USERNAME}>
                 <Form.Control
                   type="text"
                   placeholder="Username"
                   value={username}
+                  onFocus={() => setActiveControl(CONTROL_ID_USERNAME)}
                   onChange={(e) => setUsername(e.target.value)}
                 />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="password">
+              <Form.Group className="mb-3" controlId={CONTROL_ID_PASSWORD}>
                 <Form.Control
                   type="password"
                   placeholder="Password"
                   value={password}
+                  onFocus={() => setActiveControl(CONTROL_ID_PASSWORD)}
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </Form.Group>
             </Tab>
             <Tab eventKey={TAB_REGISTER} title="Register">
-              <Form.Group className="mb-3" controlId="newUsername">
+              <Form.Group className="mb-3" controlId={CONTROL_ID_NEW_USERNAME}>
                 <Form.Control
                   type="text"
                   placeholder="Username"
                   value={username}
+                  onFocus={() => setActiveControl(CONTROL_ID_NEW_USERNAME)}
                   onChange={(e) => setUsername(e.target.value)}
                   isInvalid={username.length > 0 && !validateUsername(username)}
                 />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="newPassword">
+              <Form.Group className="mb-3" controlId={CONTROL_ID_NEW_PASSWORD}>
                 <Form.Control
                   type="password"
                   placeholder="Password"
                   value={password}
+                  onFocus={() => setActiveControl(CONTROL_ID_NEW_PASSWORD)}
                   onChange={(e) => setPassword(e.target.value)}
                   isInvalid={password.length > 0 && !validatePassword(password)}
                 />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="confirmPassword">
+              <Form.Group className="mb-3" controlId={CONTROL_ID_CONFIRM_PASSWORD}>
                 <Form.Control
                   type="password"
                   placeholder="Confirm Password"
                   value={confirmPassword}
+                  onFocus={() => setActiveControl(CONTROL_ID_CONFIRM_PASSWORD)}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                   isInvalid={password !== confirmPassword}
                 />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="email">
+              <Form.Group className="mb-3" controlId={CONTROL_ID_EMAIL}>
                 <Form.Control
                   type="text"
                   placeholder="Email"
                   value={email}
+                  onFocus={() => setActiveControl(CONTROL_ID_EMAIL)}
                   onChange={(e) => setEmail(e.target.value)}
                   isInvalid={email.length > 0 && !validateEmail(email)}
                 />
               </Form.Group>
+              <RequirementsTooltip focusedControlId={activeControl} controlId={CONTROL_ID_NEW_USERNAME}>
+                <div className="text-start lh-small">
+                • 4 - 32 characters long<br />
+                • No special characters besides - and _
+                </div>
+              </RequirementsTooltip>
+              <RequirementsTooltip focusedControlId={activeControl} controlId={CONTROL_ID_NEW_PASSWORD}>
+                <div className="text-start lh-small">
+                • 8 - 32 characters long
+                </div>
+              </RequirementsTooltip>
             </Tab>
           </Tabs>
         </Form>
