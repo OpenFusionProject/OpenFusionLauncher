fee141cd318eeff5c182a5e5552247027cfa574a
Gent Semaj <me@ycc.dev>
2024-12-30 02:25:47 -0500

    Don't remove button text when button loading

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -21,11 +21,24 @@ export default function Button({
 }) {
   const actualVariant = variant ?? "primary";
   const actuallyEnabled = (enabled ?? true) && !loading;
-  const tooltipText = tooltip ?? text?.trim(); 
+  const tooltipText = tooltip ?? text?.trim();
+
+  const iconElement = loading ? (
+    <div className="spinner-border spinner-border-sm" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  ) : (
+    <i className={"fa fa-" + icon}></i>
+  );
   return (
     <button
       type="button"
-      className={(className ? (className + " ") : "") + "btn px-3 btn-" + actualVariant + (actuallyEnabled ? "" : " disabled")}
+      className={
+        (className ? className + " " : "") +
+        "btn px-3 btn-" +
+        actualVariant +
+        (actuallyEnabled ? "" : " disabled")
+      }
       data-bs-toggle="tooltip"
       data-bs-placement="top"
       disabled={!actuallyEnabled}
@@ -33,19 +46,13 @@ export default function Button({
       title={tooltipText}
       onClick={onClick}
     >
-      {loading ? (
-        <div className="spinner-border spinner-border-sm" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      ) : (
-        <>
-          {iconLeft && icon && <i className={"fa fa-" + icon}></i>}
-          {iconLeft && icon && text && " "}
-          {text && <span>{text}</span>}
-          {!iconLeft && icon && text && " "}
-          {!iconLeft && icon && <i className={"fa fa-" + icon}></i>}
-        </>
-      )}
+      <>
+        {iconLeft && icon && iconElement}
+        {iconLeft && icon && text && " "}
+        {text && <span>{text}</span>}
+        {!iconLeft && icon && text && " "}
+        {!iconLeft && icon && iconElement}
+      </>
     </button>
-  )
+  );
 }
