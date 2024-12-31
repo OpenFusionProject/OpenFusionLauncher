80af591105454f4e33e784b3d1e8fb9e82409755
Gent Semaj <me@ycc.dev>
2024-12-30 23:21:24 -0500

    Add tauri dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -963,6 +963,8 @@ async fn is_debug_mode() -> bool {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
@@ -976,7 +978,6 @@ pub fn run() {
                         .build(),
                 )?;
             }
-            app.handle().plugin(tauri_plugin_shell::init())?;
 
             state::init_app_statics(app);
             info!("OpenFusion Launcher v{}", get_app_statics().get_version());
