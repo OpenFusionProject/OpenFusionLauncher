d0f40d9ce492390a6680094e9e853ea5063a01db
HichemF <fantar.hichem@gmail.com>
2025-10-16 16:26:06 +0200

    explicit export path instead of relying on default

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+// https://v2.tauri.app/start/frontend/nextjs/
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "out",
   images: {
     unoptimized: true,
   },
